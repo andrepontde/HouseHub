99869c53806cb25eb49f86c44ae60258ad6c752c
JordanTheCodeless <131815445+JordanTheCodeless@users.noreply.github.com>
2025-02-13 21:54:12 +0000

    Changes to backend and frontend connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
-// app.get("/", (req,res) =>{
-//     res.render("../househub/public/index")
-// })
-app.listen(port, async () => {
-    console.log(`http://localhost:${port}/`);
+
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Catch-all route to serve index.html for React routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../househub/public", "index.html"));
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
