605df2781b7f42358df7a2b041d019c2e88ca998
Whitedyl <dylancs_white@hotmail.com>
2025-02-22 20:17:19 +0000

    Updated models and routes

--- backend/models/memosModel.js
+++ backend/models/memosModel.js
@@ -2,15 +2,28 @@ const mongoose = require('mongoose');
 
 //Defining the schema for a memo. (This is not done and was more of a test to see how it works and how its saving to the db)
 const memoSchema = new mongoose.Schema({
-  id: {
-    type: String
+  memoID: {
+    type: String,
+    require: true,
+    unique: true
   },
   title: {
-    type: String
+    type: String,
+    require: true
   },
   content:{ 
-    type : String
-  }
+    type : String,
+    require: true 
+  },
+  houseID: {
+    type: String,
+    require : true,
+    ref: 'House'
+    }
 });
 
 module.exports = mongoose.model('Memo', memoSchema);//exporting model to be used in routes
+
+//userID? generate on create
+//house ID (ref both)
+//
--- backend/routes/memosRoutes.js
+++ backend/routes/memosRoutes.js
@@ -1,70 +1,71 @@
 const express = require("express");
 const router = express.Router();
-const Memo = require("../models/memosModel.js");//inporting file(including mongoose)
+const Memo = require("../models/memosModel.js"); //inporting file(including mongoose)
 
 //Creating a route for new memo
-router.post("/memo", async (req,res) => {
-    try{
-        const{id, title, content} = req.body;
-        const newMemo = new Memo({id, title, content});//creting the memo instance, from data collected from body request
-        await newMemo.save(); //saving memo to database using save method from mongoose
-        res.status(201).json(newMemo);//response for created memo
-    } catch(error) {
-        res.status(500).json({error: error.message}); //response for an error
-    }
+router.post("/memo", async (req, res) => {
+  try {
+    const { memoID, title, content } = req.body;
+    const newMemo = new Memo({ memoID, title, content }); //creting the memo instance, from data collected from body request
+    await newMemo.save(); //saving memo to database using save method from mongoose
+    res.status(201).json(newMemo); //response for created memo
+  } catch (error) {
+    res.status(500).json({ error: error.message }); //response for an error
+  }
 });
 
 //Route to retrieve all memos
-router.get("/memo", async (req,res) => {
-    try {
-        const memos = await Memo.find();//fetching all memos from database
-        res.json(memos);//send the memos as a json response
-    } catch (error) {
-        res.status(500).json({error: error.message}); //response for an error
-    }
+router.get("/memo", async (req, res) => {
+  try {
+    const memos = await Memo.find(); //fetching all memos from database
+    res.json(memos); //send the memos as a json response
+  } catch (error) {
+    res.status(500).json({ error: error.message }); //response for an error
+  }
 });
 //Route to retrieve a specific memo
-router.get("/memo/:id", async (req, res) => {
+router.get("/memo/:memoID", async (req, res) => {
   try {
-    const memo = await Memo.findById(req.params.id); //fetching a memo by object id from database
+    const memo = await Memo.findOne({ memoID: req.params.memoID }); //fetching a memo by memoID from database
+    if (!memo) {
+      return res.status(404).json({ message: "Memo not found" }); //response for a memo not found
+    }
     res.json(memo); //send the memo as a json response
   } catch (error) {
     res.status(500).json({ error: error.message }); //response for an error
   }
 });
 //Updating a memo
-router.put("/memo/:id", async (req, res) => {
-    try {
-        const {id, title, content} = req.body; //taking data from body request
-        const updatedMemo = {id, title, content}; //creating new updated memo
-        const memo = await Memo.findByIdAndUpdate(req.params.id, updatedMemo, {new: true}); //updating previous memo by id from database
-        res.json(memo); //send the updated memo as a json response
-        console.log("Updated memo: " + memo);//logging if memo updated
-    } catch(error) {
-        res.status(500).json({error: error.message}); //response for an error
-    }
+router.put("/memo/:memoID", async (req, res) => {
+  try {
+    const { memoID, title, content } = req.body; //taking data from body request
+    const updatedMemo = { memoID, title, content }; //creating new updated memo
+    const memo = await Memo.findOneAndUpdate(
+      { memoID: req.params.memoID },
+      updatedMemo,
+      { new: true }
+    ); //updating previous memo by memoID from database
+    res.json(memo); //send the updated memo as a json response
+    console.log("Updated memo: " + memo); //logging if memo updated
+  } catch (error) {
+    res.status(500).json({ error: error.message }); //response for an error
+  }
 });
 //Deleting a memo
-router.delete("/memo/:id", async (req, res) => {
-    try{    
-        const memo = await Memo.findByIdAndDelete(req.params.id); //deleting a memo by id from database
-        res.json(memo); //send the deleted memo as a json response
-        console.log("Deleted memo: " + memo);//logging if memo deleted
-    } catch(error) {
-        res.status(500).json({error: error.message}); //response for an error
+router.delete("/memo/:memoID", async (req, res) => {
+  try {
+    const memo = await Memo.findOneAndDelete({ memoID: req.params.memoID }); //deleting a memo by id from database
+    if (!memo) {
+      return res.status(404).json({ message: "Memo not found" }); //response for a memo not found
     }
+    res.json(memo); //send the deleted memo as a json response
+    console.log("Deleted memo: " + memo); //logging if memo deleted
+  } catch (error) {
+    res.status(500).json({ error: error.message }); //response for an error
+  }
 });
 
 module.exports = router; //export the routes to be used in the server.js
-
-
-
-
-
-
-
-
-
 
 //left this here in case you want to use it
 
@@ -77,7 +78,7 @@ module.exports = router; //export the routes to be used in the server.js
 //     fs.readFile(housePath, 'utf-8', (error, data) => {
 //         if (error) {
 //             console.log('Error reading' + error);
-//             return res.send({status: "Failed to read"}); 
+//             return res.send({status: "Failed to read"});
 //         }
 //         let houseInfo = [];
 //         //If data exists, parse it into an array
