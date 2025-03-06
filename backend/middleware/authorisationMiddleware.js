b3eada77743555f8a244eccdb43cc80224bc5d98
Whitedyl <148541126+Whitedyl@users.noreply.github.com>
2025-03-06 13:35:53 +0000

    updated bill tracker model

--- backend/middleware/authorisationMiddleware.js
+++ backend/middleware/authorisationMiddleware.js
@@ -10,7 +10,7 @@ const authorise = async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JTW_SECRET);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.userid = decoded.userid;
       req.houseID = decoded.houseID;
       next();
